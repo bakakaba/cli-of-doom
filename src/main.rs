abf537c4d1e4b9cbecbeffbeda2887b9508f63e5
Lee Lian Hoy <cruz@zyk3.net>
2019-11-28 18:58:33 +0900

    implement cgol

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,114 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+extern crate clap;
+
+use clap::{App, Arg};
+use console::style;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::{Client, ContentLength, ContentType, Url};
 
 fn main() {
-    let stdout = stdout();
-    let out = b"Hello fellow Rustaceans! wowza";
-    let width = 80;
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    let matches = App::new("cli-of-d00m")
+        .version("0.1.0")
+        .author("Lian Hoy Lee <cruz@zyk3.net>")
+        .about("You know you're in trouble just by the name.")
+        .arg(
+            Arg::with_name("URL")
+                .required(true)
+                .takes_value(true)
+                .index(1)
+                .help("url to download"),
+        )
+        .get_matches();
+    let url = matches.value_of("URL").unwrap();
+    println!("{}", url);
+}
+
+fn create_progress_bar(quiet_mode: bool, msg: &str, length: Option<u64>) -> ProgressBar {
+    let bar = match quiet_mode {
+        true => ProgressBar::hidden(),
+        false => match length {
+            Some(len) => ProgressBar::new(len),
+            None => ProgressBar::new_spinner(),
+        },
+    };
+
+    bar.set_message(msg);
+    match length.is_some() {
+        true => bar
+            .set_style(ProgressStyle::default_bar()
+                .template("{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} eta: {eta}")
+                .progress_chars("=> ")),
+        false => bar.set_style(ProgressStyle::default_spinner()),
+    };
+
+    bar
+}
+
+fn download(target: &str, quiet_mode: bool) -> Result<(), Box<dyn (::std::error::Error)>> {
+    // parse url
+    let url = Url::parse(target)?;
+    let client = Client::new();
+    let mut resp = client.get(url).send()?;
+
+    // print(
+    //     format!(
+    //         "HTTP request sent... {}",
+    //         style(format!("{}", resp.status())).green()
+    //     ),
+    //     quiet_mode,
+    // );
+    if resp.status().is_success() {
+        let headers = resp.headers().clone();
+        let ct_len = headers.get::<ContentLength>().map(|ct_len| **ct_len);
+
+        let ct_type = headers.get::<ContentType>().unwrap();
+
+        match ct_len {
+            Some(len) => {
+                // print(
+                //     format!(
+                //         "Length: {} ({})",
+                //         style(len).green(),
+                //         style(format!("{}", HumanBytes(len))).red()
+                //     ),
+                //     quiet_mode,
+                // );
+            }
+            None => {
+                // print(format!("Length: {}", style("unknown").red()), quiet_mode);
+            }
+        }
+
+        // print(format!("Type: {}", style(ct_type).green()), quiet_mode);
+
+        let fname = target.split("/").last().unwrap();
+
+        // print(format!("Saving to: {}", style(fname).green()), quiet_mode);
+
+        let chunk_size = match ct_len {
+            Some(x) => x as usize / 99,
+            None => 1024usize, // default chunk size
+        };
+
+        let mut buf = Vec::new();
+
+        let bar = create_progress_bar(quiet_mode, fname, ct_len);
+
+        loop {
+            let mut buffer = vec![0; chunk_size];
+            let bcount = resp.read(&mut buffer[..]).unwrap();
+            buffer.truncate(bcount);
+            if !buffer.is_empty() {
+                buf.extend(buffer.into_boxed_slice().into_vec().iter().cloned());
+                bar.inc(bcount as u64);
+            } else {
+                break;
+            }
+        }
+
+        bar.finish();
+
+        save_to_file(&mut buf, fname)?;
+    }
+
+    Ok(())
 }
