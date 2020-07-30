11e8a1acdd0698622507c16bb4dccd1ba4d91123
Lee Lian Hoy <cruz@zyk3.net>
2020-07-30 09:05:46 +0900

    add structs examples

--- src/main.rs
+++ src/main.rs
@@ -218,6 +218,71 @@ fn _slice() {
     println!("first word={}", word);
 }
 
+fn _structs() {
+    struct User {
+        email: String,
+    }
+
+    // Immutable instance
+    let user1 = User {
+        email: String::from("someone@example.com"),
+    };
+
+    // Mutable instance
+    let mut user2 = User {
+        email: String::from("someone@example.com"),
+    };
+
+    user2.email = String::from("anotheremail@example.com");
+
+    // Spread create
+    let _user3 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    // Tuple structs
+    struct Color(i32, i32, i32);
+    struct Point(i32, i32, i32);
+
+    let _black = Color(0, 0, 0);
+    let _origin = Point(0, 0, 0);
+
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    fn area(rectangle: &Rectangle) -> u32 {
+        rectangle.width * rectangle.height
+    }
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    println!("rect1 is {:?}", rect1);
+    println!("rect1 is {:#?}", rect1);
+
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+    }
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
+
 fn main() {
     // let matches = App::new("cli-of-d00m")
     //     .version("0.1.0")
@@ -249,5 +314,7 @@ fn main() {
 
     // _references();
 
-    _slice();
+    // _slice();
+
+    _structs();
 }
