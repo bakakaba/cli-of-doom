f23f55a021903ea58ac518050a67e02265c7aeca
Lee Lian Hoy <cruz@zyk3.net>
2020-08-26 19:11:06 +0900

    add enum match examples

--- src/main.rs
+++ src/main.rs
@@ -311,6 +311,70 @@ fn _structs() {
     let _square1 = Rectangle::square(50);
 }
 
+fn _enums() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
+
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let _home = IpAddr::V4(127, 0, 0, 1);
+    let _loopback = IpAddr::V6(String::from("::1"));
+
+    enum Message {
+        _Quit,
+        _Move { x: i32, y: i32 },
+        Write(String),
+        _ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn callz(&self) {
+            match self {
+                Message::Write(value) => println!("message={}", value),
+                _ => println!("unsupported")
+            }
+
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.callz();
+
+    let _some_number = Some(5);
+    let _some_string = Some("a string");
+
+    let _absent_number: Option<i32> = None;
+
+    enum Coin {
+        Penny,
+        _Nickel,
+        _Dime,
+        _Quarter,
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin::_Nickel => 5,
+            Coin::_Dime => 10,
+            Coin::_Quarter => 25,
+        }
+    }
+
+    value_in_cents(Coin::Penny);
+}
+
 fn main() {
     // let matches = App::new("cli-of-d00m")
     //     .version("0.1.0")
@@ -345,4 +409,6 @@ fn main() {
     // _slice();
 
     // _structs();
+
+    _enums();
 }
