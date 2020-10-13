6d2d06a7f8859bf03aa96cf73daf07ff8af1e91a
Lee Lian Hoy <cruz@zyk3.net>
2020-10-13 11:39:53 +0900

    add traits chapter

--- src/learn/traits.rs
+++ src/learn/traits.rs
@@ -1,7 +1,13 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+
 use colored::Colorize;
 
 pub trait Summary {
     fn summarize(&self) -> String;
+    fn default_summary(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 pub struct NewsArticle {
@@ -30,17 +36,105 @@ impl Summary for Tweet {
     }
 }
 
-pub fn traits() {
-    println!("\n{}", "Traits".green().bold());
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
 
-    let tweet = Tweet {
+fn notify2<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn notify3(item: &(impl Summary + Display)) {
+    println!("Composed {}", item.summarize());
+}
+
+fn some_function<T, U>(_t: &T, _u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    0
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
         username: String::from("horse_ebooks"),
         content: String::from(
             "of course, as you probably already know, people",
         ),
         reply: false,
         retweet: false,
-    };
+    }
+}
 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+pub fn traits() {
+    println!("\n{}", "Traits".green().bold());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
     println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+    println!("Default summary! {}", article.default_summary());
+
+    notify(&article);
+    notify2(&article);
+
+    let result = returns_summarizable();
+    println!("Result summary {}", result.summarize());
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
