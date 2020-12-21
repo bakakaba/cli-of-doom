d1ecd9f2da501e076de6c23e816c91b4c7e57e57
Lee Lian Hoy <cruz@zyk3.net>
2020-12-21 19:12:48 +0900

    add learn as a subcommand

--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -1,12 +1,19 @@
-#[allow(unused_imports)]
-use learn::learn;
+use clap::{load_yaml, App};
 use dotenv::dotenv;
-
-// mod grep;
+use learn::learn;
 
 fn main() {
     dotenv().ok();
 
-    learn::run();
-    // grep::run();
+    // learn::run();
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+
+    match matches.subcommand_name() {
+        Some("learn") => {
+            learn::run();
+        }
+        _ => println!("Unknown subcommand")
+    }
+    println!("{:?}", matches);
 }
