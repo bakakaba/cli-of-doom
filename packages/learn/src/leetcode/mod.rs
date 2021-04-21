8be145a8d460ea9979e4a51642cd866ea0c41c20
Lee Lian Hoy <cruz@zyk3.net>
2021-04-21 17:24:32 +0900

    add num_pairs_divisible_by60

--- packages/learn/src/leetcode/mod.rs
+++ packages/learn/src/leetcode/mod.rs
@@ -1,2 +1,3 @@
 mod add_two_numbers;
 mod count_and_say;
+mod num_pairs_divisible_by60;
---
+++ packages/learn/src/leetcode/num_pairs_divisible_by60.rs
@@ -0,0 +1,24 @@
+#[allow(dead_code)]
+pub fn num_pairs_divisible_by60(time: Vec<i32>) -> i32 {
+    let mut remainders = [0; 60];
+    let mut count = 0;
+
+    for t in time {
+        let r = t % 60;
+        if r == 0 {
+            count += remainders[0];
+        } else {
+            count += remainders[(60 - r) as usize];
+        }
+        remainders[r as usize] += 1;
+    }
+
+    count
+}
+
+#[test]
+fn it_num_pairs_divisible_by60() {
+    let result = num_pairs_divisible_by60(vec![30, 20, 150, 100, 40]);
+    println!("{:?}", result);
+    assert_eq!(3, result);
+}
